70b129f7dcc9eda40fa99206c63b05d14aa519f3
AgilaSubramanian <96234346+AgilaSubramanian@users.noreply.github.com>
2022-01-25 12:55:17 +0530

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('sidebar_compose').addEventListener('click',function(){
+    document.querySelector('.popup').style.display = 'flex';
+})
+
+
+document.querySelector('.cancel').addEventListener('click',function(){
+document.querySelector('.popup').style.display = 'none';
+}
+)
